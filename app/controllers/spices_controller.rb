06876fb4f21a37808d2cdc5a427dd6cbe48ac47e
KellyMark <kmark602@gmail.com>
2023-08-09 16:11:53 -0400

    commit tree e57d4d3fa96075bdffbc79a50296c6e6f544c3cc

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,43 @@
+class SpicesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :spice_not_found
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    # def show
+    #     spice = find_spice
+    #     render json: spice
+    # end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        spice = find_spice
+        spice.destroy
+        head :no_content
+    end
+
+    private
+    
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def find_spice
+        Spice.find(params[:id])
+    end
+
+    def spice_not_found
+        render json: {error: "Spice not found"}, status: :not_found
+    end
+end
